9f41880edf1e79f5beef69010ee1d55cd9129f98
Squidly271 <unraidsquid@gmail.com>
2021-09-18 21:22:03 -0400

    do the watusi

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/css.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/css.php
@@ -324,13 +324,13 @@ p {margin:auto;text-align:left;margin-bottom:10px;} /* override dynamix styling 
 i.popupIcon {color:#626868;font-size:14.4rem;padding-left:1rem;}
 img.popupIcon {width:14.4rem;height:14.4rem;padding:0.3rem;border-radius:1rem 1rem 1rem 1rem;}
 .popupInfo{position:absolute;top:10rem;left:22rem;}
-.popupInfoLeft{min-width:45%;max-width:45%;float:left;display:inline-block;margin-right:10px;}
+.popupInfoLeft{min-width:45%;max-width:45%;width:50rem;float:left;display:inline-block;margin-right:10px;}
 .popupInfoRight{min-width:45%;max-width:45%;float:left;display:inline-block;}
 .popupInfoSection{line-height:2rem;font-size:1.5rem;display:inline-block;}
 .popUpLink {cursor:pointer;color:<?$aColor?>;}
 a.popUpLink {text-decoration:none;}
 a.popUpLink:hover {color:<?=$unRaid66color?>;}
-.popupName{display:inline-block;font-size:3rem;line-height:3rem;font-weight:bold;}
+.popupName{display:block;font-size:3rem;line-height:3rem;font-weight:bold;}
 
 .popupProfile {cursor:pointer;margin-top:0.5rem;display:inline-block;background: <?=$supportPopupBackground?>;background: -webkit-linear-gradient(top, transparent 0%, rgba(0,0,0,0.4) 100%),-webkit-linear-gradient(left, lighten(<?=$donateBackground?>, 15%) 0%, <?=$donateBackground?> 50%, lighten(<?=$donateBackground?>, 15%) 100%);  background: linear-gradient(to bottom, transparent 0%, rgba(0,0,0,0.4) 100%),linear-gradient(to right, lighten(#E68321, 15%) 0%, #E68321 50%, lighten(#E68321, 15%) 100%);  background-position: 0 0;  background-size: 200% 100%;  border-radius: 15px;  color: #000000;  padding: 1px 10px 1px 10px;}
 .popupProfile:hover {text-decoration:none;background-color:<?=$unRaid66color?>;}
