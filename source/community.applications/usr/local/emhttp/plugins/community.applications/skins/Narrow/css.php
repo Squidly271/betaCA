ec0e50a9640c3199063819cac1b5dd3b2fdd5fd4
Squidly271 <unraidsquid@gmail.com>
2021-10-02 13:29:47 -0400

    css

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/css.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/css.php
@@ -258,16 +258,17 @@ img.displayIcon {height:6.4rem;width:6.4rem;border-radius:1rem 1rem 1rem 1rem;}
 .docker::after{font-family:'Unraid';content:'\e90b';font-size:2.5rem;}
 .dockerHubStar {font-size:1rem;}
 .dockerHubStar::before{content:"\e95a";font-family:UnRaid;}
-.donate {background: <?=$donateBackground?>;background: -webkit-linear-gradient(top, transparent 0%, rgba(0,0,0,0.4) 100%),-webkit-linear-gradient(left, lighten(<?=$donateBackground?>, 15%) 0%, <?=$donateBackground?> 50%, lighten(<?=$donateBackground?>, 15%) 100%);  background: linear-gradient(to bottom, transparent 0%, rgba(0,0,0,0.4) 100%),linear-gradient(to right, lighten(#E68321, 15%) 0%, #E68321 50%, lighten(#E68321, 15%) 100%);  background-position: 0 0;  background-size: 200% 100%;  border-radius: 15px;  color: #000000;  padding: 1px 10px 1px 10px;}
+.donate {color:<?=$supportPopupText?>!important;background: <?=$supportPopupBackground?>;background: -webkit-linear-gradient(top, transparent 0%, rgba(0,0,0,0.4) 100%),-webkit-linear-gradient(left, lighten(<?=$donateBackground?>, 15%) 0%, <?=$donateBackground?> 50%, lighten(<?=$donateBackground?>, 15%) 100%);  background: linear-gradient(to bottom, transparent 0%, rgba(0,0,0,0.4) 100%),linear-gradient(to right, lighten(#E68321, 15%) 0%, #E68321 50%, lighten(#E68321, 15%) 100%);  background-position: 0 0;  background-size: 200% 100%;  border-radius: 15px;  color: #000000;  padding: 1px 10px 1px 10px;}
+
 .donate:hover {text-decoration:none;background-color:<?=$unRaid66color?>;}
 .donateDiv{margin-top:1rem;}
-.donate a {text-decoration:none;color:<?=$donateText?>;}
+.donate a {text-decoration:none;color:<?=$supportPopupText?>}
 .donateArea{margin-top:2rem;}
 .donateLink {font-size:1.2rem;}
 .donateText{margin-top:2rem;}
 .enabledIcon {cursor:pointer;color:<?=$unRaid66color?> !important;}
-.fav{background-color:#009900;}
-.nonfav{background-color: <?=$supportPopupBackground?>;}
+.fav{background-color:#009900;margin-left:1rem;}
+.nonfav{background-color: <?=$supportPopupBackground?>;margin-left:1rem;}
 .favCardBackground{clip-path:polygon(16% 0, 0 0, 0 19%);background-color:#c00d0d;width:10rem;height:10rem;top:0;left:-29rem;position:relative;margin-right:-10rem;}
 
 .homeMore{color:<?=$unRaid66color?>;cursor:pointer;}
